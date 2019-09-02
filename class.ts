55330f6b1441eb5fada8d912769df142963943e0
jesusharaujo <jesusharaujo@gmail.com>
2019-09-02 14:37:57 -0600

    Clases con modificadores

---
+++ class.ts
@@ -0,0 +1,10 @@
+class Shape {
+
+    area: number;
+    color: string;
+
+    constructor ( name: string, width: number, height: number ) {
+        this.area = width * height;
+        this.color = "pink";
+    };
+}
