0b89011da48f1ac71bd4c95fa6b6854ccd559822
jesusharaujo <jesusharaujo@gmail.com>
2019-09-06 15:46:25 -0600

    private

--- class.ts
+++ class.ts
@@ -1,9 +1,9 @@
 class Shape {
 
     area: number;
-    color: string;
+    private color: string;
 
-    constructor ( name: string, width: number, height: number ) {
+    constructor ( public name: string, width: number, height: number ) {
         this.area = width * height;
         this.color = "pink";
     };
