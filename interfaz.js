eeefc764e8fb15d545075f1bd831df8d89aec765
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 21:35:44 -0600

    interfaces

---
+++ interfaz.js
@@ -0,0 +1,9 @@
+var firstLake = {
+    name: 'Caspian Sea',
+    length: 1199,
+    depth: 1025,
+    area: 371000,
+    isFreshwater: false,
+    countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
+};
+console.log(firstLake);
---
+++ interfaz.ts
@@ -0,0 +1,20 @@
+interface Lakes {
+
+    name: string,
+    area: number,
+    length: number,
+    depth: number,
+    isFreshwater: boolean,
+    countries: string[]
+}
+
+let firstLake: Lakes = {
+    name: 'Caspian Sea',
+    length: 1199,
+    depth: 1025,
+    area: 371000,
+    isFreshwater: false,
+    countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
+}
+
+console.log(firstLake);
