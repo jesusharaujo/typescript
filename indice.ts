dc8d58f7e9377449171d31c7985af6bb4736cecf
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 21:49:01 -0600

    Ejemplo de indice para interfaces

---
+++ indice.ts
@@ -0,0 +1,22 @@
+interface Lakes {
+    name: string,
+    area: number,
+    length: number,
+    depth: number,
+    isFreshwater: boolean,
+    countries: string[],
+    frozen?: string[],
+    [extraProp: string]: any
+}
+   
+let fourthLake: Lakes = {
+    name: 'Tanganyika',
+    depth: 1470,
+    length: 676,
+    area: 32600,
+    isFreshwater: true,
+    countries: ['Burundi', 'Tanzania', 'Zambia', 'Congo'],
+    kigoma:'Tanzania',
+    kalemie: 'Congo',
+    bujumbura: 'Burundi'
+}
--- interfaz.ts
+++ interfaz.ts
@@ -1,20 +1,29 @@
 interface Lakes {
-
     name: string,
     area: number,
     length: number,
     depth: number,
     isFreshwater: boolean,
-    countries: string[]
+    countries: string[],
+    frozen?: string[]
+}
+   
+let secondLake: Lakes = {
+    name: 'Superior',
+    depth: 406.3,
+    length: 616,
+    area: 82100,
+    isFreshwater: true,
+    countries: ['Canada', 'United States']
+}
+    
+let thirdLake: Lakes = {
+    name: 'Baikal',
+    depth: 1637,
+    length: 636,
+    area: 31500,
+    isFreshwater: true,
+    countries: ['Russia'],
+    frozen: ['January', 'February', 'March', 'April', 'May']
 }
 
-let firstLake: Lakes = {
-    name: 'Caspian Sea',
-    length: 1199,
-    depth: 1025,
-    area: 371000,
-    isFreshwater: false,
-    countries: ['Kazakhstan', 'Russia', 'Turkmenistan', 'Azerbaijan', 'Iran']
-}
-
-console.log(firstLake);
