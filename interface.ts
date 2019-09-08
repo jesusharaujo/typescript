b45b81b42b114b8fedc7fbba2500e1ac69c66ea3
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 21:39:43 -0600

    Ejemplo de variable opcional frozen?

--- interface.ts
+++ interface.ts
@@ -10,5 +10,7 @@ function area(shape : Shape) {
     return "I'm " + shape.name + " with area " + area + " cm squared";
 }
 
+
+
 //console.log( area( {name: "rectangle", width: 30, height: 15} ) );
 console.log( area( {name: "square", width: 30, height: 30, color: "blue"} ) );
