3b2e1b53ab53bc707204953869d5834746f0384d
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 22:27:20 -0600

    Random con tipo de dato Any

---
+++ randomany.ts
@@ -0,0 +1,8 @@
+function randomElem(theArray: any[]): any {
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+    }
+    let positions = [103, 458, 472, 458];
+    let randomPosition = randomElem(positions);
+    let colors = ['violet', 'indigo', 'blue', 'green'];
+    let randomColor = randomElem(colors);
