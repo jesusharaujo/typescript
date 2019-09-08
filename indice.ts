fe427922799cd041aad783caeff2a6de82e54e7b
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 21:50:05 -0600

    Ejemplo de firma de indice para interfaces*

--- indice.ts
+++ indice.ts
@@ -12,11 +12,11 @@ interface Lakes {
 let fourthLake: Lakes = {
     name: 'Tanganyika',
     depth: 1470,
-    length: 676,
+    length: 676, 
     area: 32600,
     isFreshwater: true,
     countries: ['Burundi', 'Tanzania', 'Zambia', 'Congo'],
     kigoma:'Tanzania',
     kalemie: 'Congo',
-    bujumbura: 'Burundi'
+    bujumbura: 'Burundi, Dubai'
 }
