e8d415be6ecff2be822c860443a7c4fd466665eb
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 22:25:58 -0600

    Ejemplo de random con cadena

---
+++ random.ts
@@ -0,0 +1,6 @@
+function randomStrElem(theArray: string[]): string {
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+    }
+    let colors: string[] = ['violet', 'indigo', 'blue', 'green'];
+    let randomColor: string = randomStrElem(colors);
