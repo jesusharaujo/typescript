319c2ab9b35859eab10ca8367301b68dfcc08d98
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 21:53:10 -0600

    propiedad readonly para variables y matrices

---
+++ readonly.ts
@@ -0,0 +1,19 @@
+interface Enemy {
+    readonly size: number,
+    health: number,
+    range: number,
+    readonly damage: number
+}
+    
+let tank: Enemy = {
+    size: 50,
+    health: 100,
+    range: 60,
+    damage: 12
+}
+
+// This is Okay
+    tank.health = 95;
+
+// Error because 'damage' is read-only.
+    tank.damage = 10;
