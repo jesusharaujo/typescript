64544245db3596c6e02c481184163f9dd8b0098f
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 22:17:47 -0600

    Ejemplo de Herencia

---
+++ herencia.ts
@@ -0,0 +1,30 @@
+class Person {
+    private name: string;
+    constructor(theName: string) {
+    this.name = theName;
+    }
+    introduceSelf() {
+    console.log("Hi, I am " + this.name + "!");
+    }
+}
+    
+class Friend extends Person {
+    yearsKnown: number;
+    constructor(name: string, yearsKnown: number) {
+    super(name);
+    this.yearsKnown = yearsKnown;
+    }
+    timeKnown() {
+    console.log("We have been friends for " + this.yearsKnown + " years.")
+    }
+}
+    
+let friendA = new Friend("Jacob", 6);
+    
+// Prints: Hi, I am Jacob!
+    
+friendA.introduceSelf();
+    
+// Prints: We have been friends for 6 years.
+    
+friendA.timeKnown();
