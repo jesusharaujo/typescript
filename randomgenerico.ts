e0deb1db904e7e3030dec17266f428a3f6ec93da
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 22:28:10 -0600

    Funcion generica que devuelve random

---
+++ randomgenerico.ts
@@ -0,0 +1,6 @@
+function randomElem<T>(theArray: T[]): T {
+    let randomIndex = Math.floor(Math.random()*theArray.length);
+    return theArray[randomIndex];
+    }
+    let colors: string[] = ['violet', 'indigo', 'blue', 'green'];
+    let randomColor: string = randomElem(colors);
