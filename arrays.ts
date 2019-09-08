6079de83c8a779797f81b4bfa337d0c38dca706c
jesusharaujo <jesusharaujo@gmail.com>
2019-09-07 21:06:14 -0600

    Ejemplos de Arrays

---
+++ arrays.ts
@@ -0,0 +1,15 @@
+// With strictNullChecks set to false
+
+let a: number[] = [1, 12, 93, 5];
+let b: string[] = ["a", "apricot", "mango"];
+let c: number[] = [1, "apple", "potato"]; // Error
+let d: Array<number> = [null, undefined, 10, 15];
+let e: Array<string> = ["pie", null, ""];
+
+// With strictNullChecks set to true
+
+let a: number[] = [1, 12, 93, 5];
+let b: string[] = ["a", "apricot", "mango"];
+let c: number[] = [1, "apple", "potato"]; // Error
+let d: Array<number> = [null, undefined, 10, 15]; // Error
+let e: Array<string> = ["pie", null, ""]; // Error
